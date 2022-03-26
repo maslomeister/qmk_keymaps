ae7ab4bf32a523756a565115a89081da9e7db97a
Maslomeister <maslomeister@gmail.com>
2022-03-26 19:54:43 +0300

    fixed f row on second half

--- crkbd/keymaps/maslomeister/keymap.c
+++ crkbd/keymaps/maslomeister/keymap.c
@@ -139,10 +139,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAMING_LOWER] = LAYOUT_split_3x5_3(\
-    KC_XXXXX,   KC_1,  KC_2, KC_3, KC______, KC______, KC______, KC______,       KC_F1, KC_F2,  KC_F3, KC_F4, KC_F5, KC_F6, \
+    KC_XXXXX,   KC_1,  KC_2, KC_3, KC______, KC______, KC______,                 KC_F1, KC_F2, KC_F3,  KC_F4, KC_F5, KC_F6, KC_XXXXX, \
             KC______, KC_4, KC______, KC______, KC______, KC______,              KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, \
-                KC______, KC______, KC______, KC______, KC______, KC______,      KC______, KC______,  KC______, KC______, KC______, KC______, \
-                                        KC______, KC______,   KC______,         KC______, KC______,   TO(_QWERTY)\
+                KC______, KC______, KC______, KC______, KC______, KC______,      KC_XXXXX, KC_XXXXX,  KC_XXXXX, KC_XXXXX, KC_XXXXX, KC_XXXXX, \
+                                        KC______, KC______,   KC______,         KC_XXXXX, KC_XXXXX,   TO(_QWERTY)\
 )
 
 };
