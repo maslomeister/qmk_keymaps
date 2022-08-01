51d1b4b3098d93965c406982e49585993c47ea26
Maslomeister <maslomeister@gmail.com>
2022-08-01 03:00:37 +0300

    update

--- crkbd/keymaps/maslomeister/keymap.c
+++ crkbd/keymaps/maslomeister/keymap.c
@@ -54,16 +54,12 @@ enum custom_keycodes {
 #define KC_RAISE RAISE
 #define KC_LA_EQL LALT_T(KC_EQL)
 
-#define NBS_LCT MT(MOD_LCTL, KC_NUBS)
-#define BSH_LAL MT(MOD_LALT, KC_BSLS)
-#define APP_RAL MT(MOD_RALT, KC_APP)
-
-#define QUO_RCT MT(MOD_RCTL, KC_QUOT)
-#define MIN_RAL MT(MOD_LALT, KC_MINUS)
-#define BSL_LAL MT(MOD_LALT, KC_BSLS)
-#define GRV_LCT MT(MOD_LCTL, KC_GRV)
-#define LBR_RGU MT(MOD_LGUI, KC_LBRC)
+// #define QUO_RCT MT(MOD_RCTL, KC_QUOT)
+// #define BSL_RAL MT(MOD_LALT, KC_BSLS)
+// #define LBR_RGU MT(MOD_LGUI, KC_LBRC)
 #define RBR_LGU MT(MOD_RGUI, KC_RBRC)
+#define MIN_LAL MT(MOD_LALT, KC_MINUS)
+#define GRV_LCT MT(MOD_LCTL, KC_GRV)
 #define BSP_RSH MT(MOD_RSFT, KC_BSPC)
 #define SPC_LSH MT(MOD_LSFT, KC_SPC)
 #define DEL_RSE LT(_RAISE, KC_DEL)
@@ -75,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_split_3x6_3(
 
-        RBR_LGU, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LBR_RGU,
+        RBR_LGU, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET,
 
-        GRV_LCT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, QUO_RCT,
+        GRV_LCT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
 
-        BSL_LAL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MIN_RAL,
+        MIN_LAL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLASH,
 
         TAB_RSE, SPC_LSH, ENT_LWR, ESC_LWR, BSP_RSH, DEL_RSE
 
@@ -87,11 +83,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT_split_3x6_3(
 
-        KC_LGUI, KC_F1, KC_F2, KC_F3, KC_F4, CLRPCK, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_RGUI,
+        KC_LGUI, EXPLORER, CHROME, VSC, TERMINAL, KC_F5, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_RGUI,
 
-        KC_LCTL, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RCTL,
+        KC_LCTL, KC_NO, KC_NO, KC_NO, SCRCUT, CLRPCK, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RCTL,
 
-        KC_LALT, KC_F9, KC_F10, KC_F11, KC_F12, LNG_SW, VSC, CHROME, TERMINAL, EXPLORER, KC_NO, KC_RALT,
+        KC_LA_EQL, KC_NO, KC_NO, KC_NO, KC_NO, LNG_SW, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RALT,
 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
@@ -99,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_split_3x6_3(
 
-        KC_LGUI, KC_PAST, KC_7, KC_8, KC_9, KC_PPLS, KC_VOLU, KC_HOME, KC_MPLY, KC_PGUP, SCRCUT, KC_RGUI,
+        KC_LGUI, KC_PAST, KC_7, KC_8, KC_9, KC_PPLS, KC_VOLU, KC_HOME, KC_MPLY, KC_PGUP, KC_NO, KC_RGUI,
 
         KC_LCTL, KC_PSLS, KC_4, KC_5, KC_6, KC_PMNS, KC_MUTE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_RCTL,
 
@@ -111,11 +107,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_split_3x6_3(
 
-        KC_LGUI, KC_NO, KC_P7, KC_P8, KC_P9, KC_NLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
-        KC_LCTL, KC_PENT, KC_P4, KC_P5, KC_P6, KC_SLCK, KC_NO, KC_NO, RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG,
+        KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_SLCK, KC_NO, RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG,
 
-        KC_LALT, KC_P0, KC_P1, KC_P2, KC_P3, KC_CAPS, KC_NO, KC_NO, RGB_VAD, RGB_SAD, RGB_HUD, RGB_MOD,
+        KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_CAPS, KC_NO, RGB_VAD, RGB_SAD, RGB_HUD, RGB_MOD,
 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
@@ -325,7 +321,6 @@ extern rgblight_config_t rgblight_config;
 #endif
 
 void keyboard_post_init_user(void) {
-    debug_enable = true;
     switchNumLock(host_keyboard_led_state().num_lock);
     if (rgblight_is_enabled()) {
         rgb_enabled = 1;
